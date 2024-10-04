696066b437443c9f8f012aa7404354626a774fe1
rlitoncs <rj_liton@hotmail.com>
2024-10-03 20:20:45 -0400

    main-file.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-console.log('main file');
-
-console.log('hello from Ann')
-console.log('hello from ralph');
-console.log('from ralph-2');
-console.log("Hello from Henry");
-console.log('r-2');
+console.log('from main');
